97fffa929cd34aa187496d556cc4759433532423
dell <dell@udhai.com>
2026-07-13 11:49:54 +0530

    Revert "Fix file paths for Vercel bundling to use process.cwd()"
    This reverts commit 56dbaadd6c6723c7b46586e7d531e716cb10adc7.

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ async function initialize() {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
-    app.use(express.static(path.join(process.cwd(), "public")));
+    app.use(express.static(path.join(__dirname, "public")));
 
     app.set("view engine", "ejs");
-    app.set("views", path.join(process.cwd(), "views"));
+    app.set("views", path.join(__dirname, "views"));
 
     app.use(expressLayouts);
     app.set("layout", "layout");
